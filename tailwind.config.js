9ea849f67a4752e1762551b8106493d79a22fbe7
Gabriel <Gabriel.bachelor@gmail.com>
2024-09-20 14:10:42 +0000

    x

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {   colors: {
+      'net-red': '#E50914',
+      'hover-red': '#C40712 ',
+    }},
+  },
+  plugins: [],
+}
+
